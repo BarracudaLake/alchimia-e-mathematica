7fb6fbc1394d81e3af8e6c4435a0ba538cf8f48c
BarracudaLake <vincentstoeckelmann@icloud.com>
2023-12-08 15:47:01 +0100

    rectangl :D

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.resizable = true
+    t.console = false
+    --t.window.icon = "textures/logo.png" --> doesnt exist
+    t.identity = "AEM"
+    t.window.title = "Alchima e mathematica"
+end
--- main.lua
+++ main.lua
@@ -1,2 +1,9 @@
 require("src.rendering")
 require("src.updating")
+
+-- this is HIGHLY FOR TESTING, i looked at how you made photon and i will put stuff in src
+
+love.draw = function()
+    love.graphics.setColor(1,1,0)
+    love.graphics.rectangle("fill",love.graphics.getWidth()/2-25,love.graphics.getHeight()/2-25,50,50)
+end
